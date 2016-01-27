1416e6597208eeabf853969b0cde53f7feadbc0e
theTwist84 <lee.foulger.94@gmail.com>
2016-01-27 05:52:58 +0000

    Progress on a progress bar (nothing about a progress bar)

--- HaloOnlineInstaller/Program.cs
+++ HaloOnlineInstaller/Program.cs
@@ -16,31 +16,43 @@ namespace Updater
         {
             string dewLoc = Path.Combine(Directory.GetCurrentDirectory(), "ElDewrito");
             System.Net.WebClient wc = new System.Net.WebClient();
-            dynamic dew = JsonConvert.DeserializeObject(wc.DownloadString("http://thetwist84.github.io/HaloOnlineModManager/basegame/base.json")); MegaApiClient client = new MegaApiClient();
+            dynamic dew = JsonConvert.DeserializeObject(wc.DownloadString("http://thetwist84.github.io/HaloOnlineModManager/game/game.json")); MegaApiClient client = new MegaApiClient();
             client.LoginAnonymous();
             string name = dew["base"].Name;
             string filename = dew["base"].Filename;
             string url = dew["base"].Url;
             Uri uri = new Uri(url);
             Stopwatch watcher = Stopwatch.StartNew();
+            FastZip fastZip = new FastZip();
+            string filter = null;
 
             if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), filename)))
             {
                 Console.WriteLine("Download started for: " + name);
                 watcher.Restart();
-                client.DownloadFile(uri, Path.Combine(Directory.GetCurrentDirectory(), filename));
+                //client.DownloadFile(uri, Path.Combine(Directory.GetCurrentDirectory(), filename));
+                client.DownloadFileAsync(uri, Path.Combine(Directory.GetCurrentDirectory(), filename));
+                Console.WriteLine(client.Progress);
                 watcher.Stop();
                 Console.WriteLine("Download finished for: " + name + " in {0}.\n ", watcher.Elapsed);
+
+                if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), filename)))
+                {
+                    Console.WriteLine("Extraction started for: " + name);
+                    watcher.Restart();
+                    fastZip.ExtractZip(filename, dewLoc, filter);
+                    watcher.Stop();
+                    Console.WriteLine("Extraction finished for: " + name + " in {0}.\n ", watcher.Elapsed);
+                }
             }
-
-            FastZip fastZip = new FastZip();
-            string filter = null;
-
-            Console.WriteLine("Extraction started for: " + name);
-            watcher.Restart();
-            fastZip.ExtractZip(filename, dewLoc, filter);
-            watcher.Stop();
-            Console.WriteLine("Extraction finished for: " + name + " in {0}.\n ", watcher.Elapsed);
+            else if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), filename)))
+            {
+                Console.WriteLine("Extraction started for: " + name);
+                watcher.Restart();
+                fastZip.ExtractZip(filename, dewLoc, filter);
+                watcher.Stop();
+                Console.WriteLine("Extraction finished for: " + name + " in {0}.\n ", watcher.Elapsed);
+            }
 
             if (!File.Exists(Path.Combine(dewLoc, "autoexec.cfg")))
             {
